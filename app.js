add4bc8b39ecbf931b20de6259db4a6e4321ab3d
k_yamashiro <yamashiro@rrr-z.co.jp>
2018-12-27 17:44:12 +0900

    computed

--- app.js
+++ app.js
@@ -3,32 +3,34 @@ let items = [
     {
         name: '鉛筆',
         price: 300,
-        quantity: 0
+        quantity: 1
     },
     {
         name: 'ノート',
         price: 400,
-        quantity: 0
+        quantity: 2
     },
     {
         name: '消しゴム',
         price: 500,
-        quantity: 0
+        quantity: 3
     }
 ];
 
-new Vue({
+let vm = new Vue({
     el: '#app',
     data: {
         items: items
     },
-    filters: {
-        numberWithDelimiter: function (value) {
-            if (!value) {
-                return 0
-            }
-
-            return value.toString().replace(/(\d)(?=(\d{3})+$)/g,'$1,')
+    computed: {
+        totalPrice: function () {
+            return this.items.reduce(
+                function (sum, item) {
+                    return sum + (item.price * item.quantity)
+                }, 0)
+        },
+        totalPriceWithTax: function () {
+            return Math.floor(this.totalPrice * 1.08)
         }
     }
 });
