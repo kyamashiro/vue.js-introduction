9de3a0c82e1245a91392518257b2555928027100
k_yamashiro <yamashiro@rrr-z.co.jp>
2018-12-28 18:00:04 +0900

    render

--- chapter3/main.js
+++ chapter3/main.js
@@ -1,26 +1,13 @@
-Vue.component('fruits-list-title', {
-    template: '<h1>フルーツ一覧</h1>',
-});
-
-Vue.component('fruits-list-description', {
-    template: '季節のフルーツ',
-});
-
-Vue.component('fruits-list-table', {
-    template: '<table>\n' +
-        '            <tr>\n' +
-        '                <th>季節</th>\n' +
-        '                <th>フルーツ</th>\n' +
-        '            </tr>\n' +
-        '            <tr>\n' +
-        '                <td>春</td>\n' +
-        '                <td>もも</td>\n' +
-        '            </tr>\n' +
-        '            <tr>\n' +
-        '                <td>夏</td>\n' +
-        '                <td>すいか</td>\n' +
-        '            </tr>\n' +
-        '        </table>'
+Vue.component('input-date-with-today', {
+    render: function (createElement) {
+        return createElement(
+            'input', {
+                attrs: {
+                    type: 'date',
+                    value: new Date().toISOString().substring(0, 10)
+                }
+            })
+    }
 });
 
 new Vue({
