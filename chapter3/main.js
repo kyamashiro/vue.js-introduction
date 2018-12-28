84f89feeec3aa8f3f6aae0b6a41101344ddae484
k_yamashiro <yamashiro@rrr-z.co.jp>
2018-12-28 17:48:39 +0900

    template

--- chapter3/main.js
+++ chapter3/main.js
@@ -1,5 +1,26 @@
-Vue.component('list-item', {
-    template: '<li>foo</li>'
+Vue.component('fruits-list-title', {
+    template: '<h1>フルーツ一覧</h1>',
+});
+
+Vue.component('fruits-list-description', {
+    template: '季節のフルーツ',
+});
+
+Vue.component('fruits-list-table', {
+    template: '<table>\n' +
+        '            <tr>\n' +
+        '                <th>季節</th>\n' +
+        '                <th>フルーツ</th>\n' +
+        '            </tr>\n' +
+        '            <tr>\n' +
+        '                <td>春</td>\n' +
+        '                <td>もも</td>\n' +
+        '            </tr>\n' +
+        '            <tr>\n' +
+        '                <td>夏</td>\n' +
+        '                <td>すいか</td>\n' +
+        '            </tr>\n' +
+        '        </table>'
 });
 
 new Vue({
