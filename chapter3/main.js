e25d006e40843dfeb821bf2c58d151850e6cb53b
kyamashiro <yamashiro@rrr-z.co.jp>
2018-12-30 09:45:05 +0900

    親から子コンポーネントへのデータの受け渡し

--- chapter3/main.js
+++ chapter3/main.js
@@ -29,3 +29,34 @@ new Vue({
         myItem: 'pen'
     }
 });
+
+
+/*
+親から子にしかデータを渡せない
+子コンポーネントでpropsを指定し､テンプレート経由で親から子にデータを渡す
+*/
+
+//子コンポーネント
+Vue.component('fruits-item-name', {
+    //親から受け取る属性
+    props: {
+        fruitsItem: {
+            type: Object,
+            required: true
+        },
+    },
+    template: '<li>{{fruitsItem.name}}</li>'
+});
+
+//親コンポーネント
+new Vue({
+    //htmlの#fruits-componentの要素
+    el: '#fruits-component',
+    data: {
+        //v-forで使う要素
+        fruitsItems: [
+            {name: '梨'},
+            {name: 'いちご'}
+        ]
+    }
+});
