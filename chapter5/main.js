56972e56f6fae075f9123af8be23fa0f0ca6c4c6
kyamashiro <yamashiro@rrr-z.co.jp>
2019-01-02 10:06:03 +0900

    カスタムトランジション

--- chapter5/main.js
+++ chapter5/main.js
@@ -2,7 +2,17 @@ new Vue({
     el: '#app',
     data: function () {
         return {
+            //inputに表示する初期テキスト
+            animationClass: 'bounce',
+            //v-showでtrue/falseで表示/非表示を切り替えるために使用するデータ
             isShown: false
+        }
+    },
+    computed: {
+        activeClass: function () {
+            // "animationClass(inputの値) + animated"をクラス名として指定する
+            //enter-active-class, leave-active-classのカスタムトランジションクラスにクラス名を付与する
+            return this.animationClass + ' animated'
         }
     }
 });
