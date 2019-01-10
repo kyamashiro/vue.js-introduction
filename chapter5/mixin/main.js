56379dd4689d734d392a340b4f0c271b8e8414c3
kyamashiro <yamashiro@rrr-z.co.jp>
2019-01-10 23:05:45 +0900

    グローバルミックスイン

--- chapter5/mixin/main.js
+++ chapter5/mixin/main.js
@@ -46,10 +46,31 @@ let TextShareButton = {
     },
 };
 
+Vue.mixin({
+    data: function () {
+        return {
+            loggedInUser: null
+        }
+    },
+    created: function () {
+        let auth = this.$options.auth;
+        this.loggedInUser = JSON.parse(sessionStorage.getItem("loggedInUser"));
+        if (auth && !this.loggedInUser) {
+            window.alert("このページはログインが必要です")
+        }
+    }
+});
+
+let LoginRequiredPage = {
+    auth:true,
+    template:"<div><p v-if='!loggedInUser'>このページはログインが必要です</p><p v-else>{{loggedInUser.Name}}さんでログインしています</p></div>"
+}
+
 new Vue({
     el: '#app',
     components: {
         IconShareButton,
-        TextShareButton
+        TextShareButton,
+        LoginRequiredPage
     }
 });
