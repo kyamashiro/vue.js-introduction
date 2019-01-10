f81ab25c61de9dd10616a4a60dac0afc0f941f85
kyamashiro <yamashiro@rrr-z.co.jp>
2019-01-10 22:45:06 +0900

    ミックスインの機能をコンポーネントに追加するには、コンポーネントオプジョンのmixinsプロパティの配列の要素として、ミックスインのオブジェクトを渡します。mixinsが配列になっているのは、コンポーネントに、複数のミックスインの追加を可能にするためです。

--- chapter5/mixin/main.js
+++ chapter5/mixin/main.js
@@ -1,6 +1,4 @@
-let IconShareButton = {
-    template:
-        '<button @click="share"><i class="fas fa-share-square"></i></button>',
+let Sharable = {
     data: function () {
         return {
             _isProcessing: false
@@ -24,28 +22,18 @@ let IconShareButton = {
     }
 };
 
+let IconShareButton = {
+    mixins: [Sharable],
+    template:
+        '<button @click="share"><i class="fas fa-share-square"></i></button>'
+};
+
 let TextShareButton = {
+    mixins: [Sharable],
     template: '<button @click="share">{{buttonLabel}}</button>',
     data: function () {
         return {
             buttonLabel: 'シェアする',
-            _isProcessing: false
-        }
-    },
-    methods: {
-        share: function () {
-            if (this._isProcessing) {
-                return
-            }
-            if (!window.confirm('シェアしますか?')) {
-                return
-            }
-            this._isProcessing = true;
-
-            setTimeout(() => {
-                window.alert('シェアしました');
-                this._isProcessing = false
-            }, 300)
         }
     }
 };
