b638e267995433bc3f96c450d9d17e3f376fc881
kyamashiro <yamashiro@rrr-z.co.jp>
2019-01-12 09:02:18 +0900

    styleのカプセル化

---
+++ chapter6/bar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bar">
+        <h1 class="header">
+            Bar コンポーネント
+            .header font-size:50%
+        </h1>
+        <p>
+            これはBarコンポーネントです
+        </p>
+    </div>
+</template>
+
+<style>
+    p {
+        text-decoration: underline;
+    }
+</style>
+
+<style scoped>
+    .bar {
+        border: solid 1px gray;
+        margin: 4px;
+        padding: 4px;
+    }
+
+    .header {
+        font-size: 50%;
+    }
+</style>
---
+++ chapter6/foo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="foo">
+        <h1 class="header">
+            Foo コンポーネント
+            .header font-size:150%
+        </h1>
+        <p>
+            これはFooコンポーネントです
+        </p>
+    </div>
+</template>
+
+<style>
+    p {
+        text-decoration: underline;
+    }
+</style>
+
+<style scoped>
+    .foo {
+        border: solid 1px green;
+        margin: 4px;
+        padding: 4px;
+    }
+
+    .header {
+        font-size: 150%;
+    }
+</style>
