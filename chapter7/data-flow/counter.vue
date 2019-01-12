fd4c943c3d51464cdef24b0f3198ac626a368daa
kyamashiro <yamashiro@rrr-z.co.jp>
2019-01-12 18:45:02 +0900

    vuex

---
+++ chapter7/data-flow/counter.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <p>{{getCount}}</p>
+        <button v-on:click="increment">+</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "counter",
+        data() {
+            return {
+                count: 0
+            }
+        },
+        computed: {
+            getCount() {
+                return this.count;
+            }
+        },
+        methods: {
+            increment() {
+                this.count += 1
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
