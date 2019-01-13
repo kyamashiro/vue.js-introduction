5bafc377325cffea5d826586b76c864a60117903
kyamashiro <yamashiro@rrr-z.co.jp>
2019-01-13 21:29:28 +0900

    アクションの雛形

---
+++ chapter8/kanban-app/src/store/actions.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import * as types from './mutation-types'
+import {Auth, List, Task} from '../api'
+/* eslint-enable no-used-vars */
+
+// アクションごとにアクションハンドラ関数を定義
+export default {
+  login: ({commit}) => {
+    // TODO
+    throw new Error('login action should be implemented')
+  },
+
+  fetchLists: ({commit}) => {
+    // TODO
+    throw new Error('fetchLists should be implemented')
+  },
+
+  addTask: ({commit}) => {
+    // TODO
+    throw new Error('addTask should be implemented')
+  },
+
+  updateTask: ({commit}) => {
+    // TODO
+    throw new Error('updateTask should be implemented')
+  },
+
+  removeTask: ({commit}) => {
+    // TODO
+    throw new Error('removeTask should be implemented')
+  },
+
+  logout: ({commit}) => {
+    // TODO
+    throw new Error('logout should be implemented')
+  }
+}
