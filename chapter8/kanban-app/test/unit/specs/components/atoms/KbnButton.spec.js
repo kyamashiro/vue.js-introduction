abb2a3b549ba306233c2d407a74ad4199995c07c
kyamashiro <yamashiro@rrr-z.co.jp>
2019-01-14 12:23:57 +0900

    ボタンのテスト作成

---
+++ chapter8/kanban-app/test/unit/specs/components/atoms/KbnButton.spec.js
@@ -0,0 +1,16 @@
+import { mount } from '@vue/test-utils'
+import KbnButton from '@/components/atoms/KbnButton.vue'
+
+describe('KbnButton', () => {
+  describe('プロパティ', () => {
+    describe('type', () => {
+      describe('デフォルト', () => {
+        it('kbn-buttonクラスを持つbutton要素で構成されること', () => {
+          const button = mount(KbnButton)
+          expect(button.is('button')).to.equal(true)
+          expect(button.classes()).to.include('kbn-button')
+        })
+      })
+    })
+  })
+})
